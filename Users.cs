7e07cc20c4e6cce400b1d9e785daffac4c57ebd1
Leenesh37 <70005026+Leenesh37@users.noreply.github.com>
2023-10-08 22:09:07 +0800

    Add files via upload

---
+++ Users.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace FreelancerAPI
+{
+    public class Users
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        public string Username { get; set; } = "";
+
+        [StringLength(50)]
+        public string Email { get; set; } = "";
+
+        [StringLength(50)]
+        public string Phone { get; set; } = "";
+
+        [StringLength(50)]
+        public string Skillsets { get; set; } = "";
+
+        [StringLength(50)]
+        public string Hobby { get; set; } = "";
+    }
+}
